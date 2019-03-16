9ab39e15a565ef6fa3df690acbb12be4e6ab1aa3
ravi kumar <91raviindia@gmail.com>
2019-03-16 11:52:47 +0530

    StringBuilder example for builder design pattern

---
+++ Design_Pattern/src/main/java/builder/StringBuilderTest.java
@@ -0,0 +1,25 @@
+package builder;
+
+public class StringBuilderTest {
+
+	public static void main(String[] args) {
+
+		StringBuilder sb = new StringBuilder();
+
+		// logic to finding name
+
+		sb.append("name");
+
+		// logic to finding address
+
+		sb.append(" address");
+
+		// logic to find phone
+
+		sb.append(" phone number");
+
+		System.out.println("sb :: " + sb);
+
+	}
+
+}
