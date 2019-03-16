9513eee5009165ab784c910915a1ff7784bdaffc
ravi kumar <91raviindia@gmail.com>
2019-03-16 11:14:31 +0530

    builder design pattern

--- Design_Pattern/src/main/java/builder/TestBuilder.java
+++ Design_Pattern/src/main/java/builder/TestBuilder.java
@@ -1,10 +1,22 @@
 package builder;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class TestBuilder {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		SpringApplication.run(TestBuilder.class, args);
+
+		// Using builder to get the object in a single line of code and
+		// without any inconsistent state or arguments management issues
+		Computer comp = new Computer.ComputerBuilder("500 GB", "2 GB").setBluetoothEnabled(true)
+				.setGraphicsCardEnabled(true).build();
+		
+		
+		System.out.println("comp :: " +comp);
 	}
 
 }
